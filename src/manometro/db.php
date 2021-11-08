f6d0ee44d8a052179d9903a13d9e9c6ddb74bf1d
= <=>
2021-11-08 19:58:32 -0400

    Cambiar a variable de entorno

--- src/manometro/db.php
+++ src/manometro/db.php
@@ -1,7 +1,8 @@
 <?php
 
 
-$con = pg_connect('host=localhost port=5432 dbname=manometro user=guina password=1234');
+//$con = pg_connect('host=localhost port=5432 dbname=manometro user=guina password=1234');
+$con = pg_connect(getenv("DATABASE_URL"));
 
 if (!$con) {
     exit(pg_last_error());
