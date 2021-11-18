ea8083e8d40e93b83405d2c531cf56a94b122300
connellguina <connellguina@gmail.com>
2021-11-18 17:42:03 -0400

    agregar collapse-medida

--- src/manometro/js/main.js
+++ src/manometro/js/main.js
@@ -1,4 +1,9 @@
 $('.collapse-pozo').on('show.bs.collapse', function (e) {
     $(this).find('input').attr('disabled', $(this).attr('id') !== $(e.target).attr('id'));
     $(this).find('textarea').attr('disabled', $(this).attr('id') !== $(e.target).attr('id'));
-})
+});
+
+$('.collapse-medida').on('show.bs.collapse', function (e) {
+    $(this).find('input').attr('disabled', $(this).attr('id') !== $(e.target).attr('id'));
+    $(this).find('textarea').attr('disabled', $(this).attr('id') !== $(e.target).attr('id'));
+});
