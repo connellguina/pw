98a3eb228378f519f4393893aee3ab0f6f201e78
connellguina <connellguina@gmail.com>
2021-12-08 11:17:18 -0400

    save dir

--- src/bloc-de-notas/save-dir.php
+++ src/bloc-de-notas/save-dir.php
@@ -4,7 +4,7 @@ session_start();
 
 if ($_POST['save-dir']) {
     if (!empty($_POST['dirname'])) {
-        if (mkdir(__DIR__.'/'.$_POST['dirname'])) {
+        if (mkdir(__DIR__."/archivos/{$_POST['dirname']}")) {
         } else {
             $_SESSION['msg'] = 'Unable to create directory';
         }
